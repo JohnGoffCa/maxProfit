7582f23b3fe9355f24596bd56a89978ec315fe26
John Goff <gygn.johngoff@gmail.com>
2017-07-27 10:24:46 -0400

    Brute force function works

---
+++ max.js
@@ -0,0 +1,35 @@
+function maxProfit(array) {
+  // if our array only has one number, we can't make any profits.
+  if (array.length <= 1) return -1;
+
+  var containsNum = false;
+  for (var i = 0; i < array.length; i++) {
+    if (!isNaN(array[i]) && (array[i] !== true || array[i] !== false)) 
+      containsNum = true;
+  }
+
+  // if our array has no numbers, we can't make any profit. 
+  if (!containsNum) return -1;
+
+  var maxProfit = 0;
+  var pair = [0, -1];
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i + 1; j < array.length; j++) {
+      var current = compare(array[i], array[j]);
+      if (current > maxProfit) {
+        maxProfit = current;
+        pair[0] = array[i];
+        pair[1] = array[j];
+      }
+    }
+  }
+  return compare(pair[0], pair[1]);
+}
+
+function compare(a, b) {
+  var result = b - a;
+  if (result < 1) return -1;
+  return result;
+}
+
+module.exports = maxProfit;
---
+++ test/test.js
@@ -0,0 +1,34 @@
+var assert = require("chai").assert;
+var maxProfit = require("../max.js");
+
+describe("Maximise Profits", function() {
+  it("Should return 16 given [45, 24, 35, 31, 40, 38, 11]", function() {
+    var arr = [45, 24, 35, 31, 40, 38, 11];
+    assert.strictEqual(maxProfit(arr), 16);
+  });
+
+  it("Should return 8 given [1, 2, 3, 4, 5, 6, 7, 8, 9]", function() {
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    assert.strictEqual(maxProfit(arr), 8);
+  });
+
+  it("Should return -1 given [9, 8, 7, 6, 5, 4, 3, 2, 1]", function() {
+    var arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+    assert.strictEqual(maxProfit(arr), -1);
+  });
+
+  it("Should return -1 given an array with a single member", function() {
+    var arr = [1];
+    assert.strictEqual(maxProfit(arr), -1);
+  });
+
+  it("Should return -1 given an array with non-numbers", function() {
+    var arr = ['a', 'b', 'c', 'd', 'e', 'f']
+    assert.strictEqual(maxProfit(arr), -1);
+  });
+
+  it("Should ignore non-numbers given a mixed array", function() {
+    var arr = [1, 3, 5, 'a', 2, true, 9];
+    assert.strictEqual(maxProfit(arr), 8);
+  });
+});
